1ee632e83f78becfec4d24425a5b9ab5941a0c77
hayden Shively <haydenshively@gmail.com>
2021-02-04 03:28:00 -0600

    Cleanup docstrings and add public getter for queue

--- services/txmanager/src/IncognitoQueue.ts
+++ services/txmanager/src/IncognitoQueue.ts
@@ -1,8 +1,6 @@
-import { TransactionReceipt as ITxReceipt } from 'web3-core';
 import { BlockHeader } from 'web3-eth';
-import Web3Utils from 'web3-utils';
 import Web3 from 'web3';
-import { Big, ITx, TxQueue, Wallet } from '@goldenagellc/web3-blocks';
+import { Big, TxQueue, Wallet } from '@goldenagellc/web3-blocks';
 
 import Treasury from './contracts/Treasury';
 import IEthSubscriptionConsumer from './types/IEthSubscriptionConsumer';
@@ -11,6 +9,27 @@ import IEthSubscriptionConsumer from './types/IEthSubscriptionConsumer';
 const winston = require('winston');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
+/*
+Goal:
+At some point in time, the current queue will contain 0 or more transactions.
+At this point in time, we may want to begin transitioning to another queue.
+
+In order to transition to a new queue, we must do the following:
+1) generate the new wallet/queue combo
+2) create a new tx on *the current queue* that calls appropriate transition function
+3) as soon as transition function is confirmed, copy all higher nonce txns from old queue to new queue
+
+step 2 can have multiple modes:
+- insertion modes (essentially choose whether transition function is `replaced` (and where) or simply appended):
+  - immediate (gets sent at lowest liquid nonce, replacing existing if necessary)
+  - end (gets appended to end of queue)
+- completion mode
+  - "lazy": the transition function tx will always be at the end of the queue (both appending and replacing bump it up a nonce)
+  - "aggressive": the transition function cannot be replaced
+
+Note: the immediate insertion mode and aggressive completion mode may be cool to have, but probably wouldn't get much use
+in the context of New Bedford. For now I'm just going to implement "end" and "lazy"
+*/
 export default class IncognitoQueue implements IEthSubscriptionConsumer {
   private active: TxQueue;
   private staged: TxQueue | null;
@@ -21,6 +40,10 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
     this.staged = null;
 
     this.gasPrice = Big('0');
+  }
+
+  public get queue(): TxQueue {
+    return this.active;
   }
 
   public get transitioning(): boolean {
@@ -36,15 +59,14 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
 
     // Check [again] that we are transitioning because of async/await and see if
     // on-chain caller matches staged address
-    // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
-    if (this.transitioning && (await Treasury.latest.caller()(provider)) === this.staged.wallet.address) {
+    // Note: `this.staged` is known to be non-null because of `this.transitioning`
+    if (this.transitioning && (await Treasury.latest.caller()(provider)) === this.staged!.wallet.address) {
       // Transition was successful:
       // - update local state
-      // - send one final transaction to clean ETH crumbs from old wallet (see further below)
       this.finishTransition();
       return;
     }
-    
+
     if (this.active.length === 0) {
       // Transition (1) hasn't started (2) failed or (3) was replaced by other tx:
       // - start it / try again
@@ -53,8 +75,8 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
       // so that we test latency on every transition (since tx speed isn't as
       // crucial on these, it's fine to send with Infura to test latency)
       const balance = Big(await this.active.wallet.getBalance());
-      // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
-      const tx = Treasury.latest.changeIdentity(this.staged.wallet.address, balance, this.gasPrice);
+      // Note: `this.staged` is known to be non-null because of `this.transitioning`
+      const tx = Treasury.latest.changeIdentity(this.staged!.wallet.address, balance, this.gasPrice);
       this.active.append(tx);
     }
   }
@@ -76,7 +98,7 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
     this.gasPrice = gasPrice;
   }
 
-  private finishTransition() {
+  private finishTransition(): void {
     // CAUTION: txns moved from one queue to another in this manner will lose their callback
     //    hooks and connection usage settings. Since these advanced options are only really
     //    really used for LatencyTester in New-Bedford, this should be ok, but be aware.
@@ -95,26 +117,4 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
 
     return new TxQueue(account.wallet);
   }
-
-  /*
-  Goal:
-  At some point in time, the current queue will contain 0 or more transactions.
-  At this point in time, we may want to begin transitioning to another queue.
-  
-  In order to transition to a new queue, we must do the following:
-  1) generate the new wallet/queue combo
-  2) create a new tx on *the current queue* that calls appropriate transition function
-  3) as soon as transition function is confirmed, copy all higher nonce txns from old queue to new queue
-
-  step 2 can have multiple modes:
-  - insertion modes (essentially choose whether transition function is `replaced` (and where) or simply appended):
-    - immediate (gets sent at lowest liquid nonce, replacing existing if necessary)
-    - end (gets appended to end of queue)
-  - completion mode
-    - "lazy": the transition function tx will always be at the end of the queue (both appending and replacing bump it up a nonce)
-    - "aggressive": the transition function cannot be replaced
-
-  Note: the immediate insertion mode and aggressive completion mode may be cool to have, but probably wouldn't get much use
-  in the context of New Bedford. For now I'm just going to implement "end" and "lazy"
-  */
 }
