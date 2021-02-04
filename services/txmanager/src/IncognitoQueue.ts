620c02689851c79f9912ea983cc6c3d9bf67edef
hayden Shively <haydenshively@gmail.com>
2021-02-04 03:22:27 -0600

    Remove complex burned crumb transfer

--- services/txmanager/src/IncognitoQueue.ts
+++ services/txmanager/src/IncognitoQueue.ts
@@ -12,13 +12,11 @@ const winston = require('winston');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 export default class IncognitoQueue implements IEthSubscriptionConsumer {
-  private readonly burned: TxQueue[];
   private active: TxQueue;
   private staged: TxQueue | null;
   private gasPrice: Big;
 
   constructor(initialWallet: Wallet) {
-    this.burned = [];
     this.active = new TxQueue(initialWallet);
     this.staged = null;
 
@@ -30,58 +28,35 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
   }
 
   public async onNewBlock(_header: BlockHeader, provider: Web3): Promise<void> {
-    if (this.transitioning) {
-      // *Must* rebase, even if only because `finishTransition` expects it. If omitted,
-      // `finishTransition` may accidentally copy confirmed txns to new queue. Here it
-      // has the welcome side-effect of making the `if else` case trigger accurately
-      await this.active.rebase();
+    if (!this.transitioning) return;
+    // *Must* rebase, even if only because `finishTransition` expects it. If omitted,
+    // `finishTransition` may accidentally copy confirmed txns to new queue. Here it
+    // has the welcome side-effect of making the second if statement trigger accurately
+    await this.active.rebase();
 
-      // Check [again] that we are transitioning because of async/await and see if
-      // on-chain caller matches staged address
-      // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
-      if (this.transitioning && (await Treasury.latest.caller()(provider)) === this.staged.wallet.address) {
-        // Transition was successful:
-        // - update local state
-        // - send one final transaction to clean ETH crumbs from old wallet (see further below)
-        this.finishTransition();
-      } else if (this.active.length === 0) {
-        // Transition (1) hasn't started (2) failed or (3) was replaced by other tx:
-        // - start it / try again
-
-        // TODO other options available on this append transition -- could set this up
-        // so that we test latency on every transition (since tx speed isn't as
-        // crucial on these, it's fine to send with Infura to test latency)
-        const balance = Big(await this.active.wallet.getBalance());
-        // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
-        const tx = Treasury.latest.changeIdentity(this.staged.wallet.address, balance, this.gasPrice);
-        this.active.append(tx);
-      }
+    // Check [again] that we are transitioning because of async/await and see if
+    // on-chain caller matches staged address
+    // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
+    if (this.transitioning && (await Treasury.latest.caller()(provider)) === this.staged.wallet.address) {
+      // Transition was successful:
+      // - update local state
+      // - send one final transaction to clean ETH crumbs from old wallet (see further below)
+      this.finishTransition();
+      return;
     }
+    
+    if (this.active.length === 0) {
+      // Transition (1) hasn't started (2) failed or (3) was replaced by other tx:
+      // - start it / try again
 
-    this.burned.forEach((q, i) => {
-      q.wallet.getBalance().then((res) => {
-        const balance = Big(res);
-        if (balance.eq('0')) {
-          this.burned.splice(i, 1);
-          return;
-        }
-
-        const tx = q.wallet.emptyTx;
-        tx.gasPrice = this.gasPrice;
-
-        // TODO assumes value will be poitive
-
-        let value = balance.minus(tx.gasPrice.mul(tx.gasLimit));
-        tx.value = Web3Utils.toHex(value.toFixed());
-        if (q.length > 0) {
-          q.replace(0, tx, 'min');
-          // Do everything again in case queue jacked up the gas price on us
-          let value = balance.minus(tx.gasPrice.mul(tx.gasLimit));
-          tx.value = Web3Utils.toHex(value.toFixed());
-          q.replace(0, tx, 'as_is');
-        } else q.append(tx);
-      });
-    });
+      // TODO other options available on this append transition -- could set this up
+      // so that we test latency on every transition (since tx speed isn't as
+      // crucial on these, it's fine to send with Infura to test latency)
+      const balance = Big(await this.active.wallet.getBalance());
+      // @ts-expect-error: `this.staged` is known to be non-null because of `this.transitioning`
+      const tx = Treasury.latest.changeIdentity(this.staged.wallet.address, balance, this.gasPrice);
+      this.active.append(tx);
+    }
   }
 
   onNewTxHash(_hash: string, _provider: Web3): void {
@@ -108,12 +83,9 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
     for (let idx = 0; idx < this.active.length; idx++) this.staged?.append(this.active.tx(idx));
 
     // update state
-    this.burned.push(this.active);
     this.active = this.staged as TxQueue;
     this.staged = null;
   }
-
-  public async beginCleanup(): Promise<void> {}
 
   private createPeer(): TxQueue {
     const account = this.active.wallet.createPeer();
@@ -133,7 +105,6 @@ export default class IncognitoQueue implements IEthSubscriptionConsumer {
   1) generate the new wallet/queue combo
   2) create a new tx on *the current queue* that calls appropriate transition function
   3) as soon as transition function is confirmed, copy all higher nonce txns from old queue to new queue
-  4) send one final tx from the old queue that makes sure its balance is 0 (to new queue or treasury)
 
   step 2 can have multiple modes:
   - insertion modes (essentially choose whether transition function is `replaced` (and where) or simply appended):
